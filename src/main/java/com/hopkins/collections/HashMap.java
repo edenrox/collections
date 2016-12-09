b9bfae9fd0a830366fa4f3508dbac6ebc0615353
Ian Hopkins <ian@lucidhelix.com>
2016-12-08 22:29:24 -0800

    Add an incomplete HashMap implemented using separate chaining.
    Also adds a HashMapTest.

--- src/main/java/com/hopkins/collections/HashMap.java
+++ src/main/java/com/hopkins/collections/HashMap.java
@@ -1,5 +1,213 @@
 package com.hopkins.collections;
 
-public class HashMap<K,V> implements Map<K,V> {
+import java.util.Objects;
+
+public class HashMap<K, V> implements Map<K, V> {
+    private static final int DEFAULT_CAPACITY = 16;
+    private static final float DEFAULT_LOAD_FACTOR = 0.75f;
     
+    private final float loadFactor;
+    
+    private HashMapEntry[] table;
+    private int size;
+    
+    public HashMap() {
+        this(DEFAULT_CAPACITY, DEFAULT_LOAD_FACTOR);
+    }
+    
+    public HashMap(int initialCapacity) {
+        this(initialCapacity, DEFAULT_LOAD_FACTOR);
+    }
+    
+    public HashMap(int initialCapacity, float loadFactor) {
+        this.table = new HashMapEntry[initialCapacity];
+        this.loadFactor = loadFactor;
+    }
+    
+    public HashMap(Map<? extends K, ? extends V> m) {
+        this();
+        putAll(m);
+    }
+    
+    int capacity() {
+        return table.length;
+    }
+
+    @Override
+    public void clear() {
+        if (isEmpty()) {
+            return;
+        }
+        size = 0;
+        for (int i = 0; i < table.length; i++) {
+            table[i] = null;
+        }
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        int position = Objects.hashCode(key) % table.length;
+        HashMapEntry<K, V> entry = table[position];
+        while (entry != null) {
+            if (Objects.equals(entry.key, key)) {
+                return true;
+            }
+            entry = entry.next;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        for (HashMapEntry<K, V> entry : table) {
+            while (entry != null) {
+                if (Objects.equals(entry.value, value)) {
+                    return true;
+                }
+                entry = entry.next;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public Set<Entry<K, V>> entrySet() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public V get(Object key) {
+        int position = Objects.hashCode(key) % table.length;
+        HashMapEntry<K, V> entry = table[position];
+        while (entry != null) {
+            if (Objects.equals(entry.key, key)) {
+                return entry.value;
+            }
+            entry = entry.next;
+        }
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public Set<K> keySet() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public V put(K key, V value) {
+        ensureCapacity(size + 1);
+        
+        int position = Objects.hashCode(key) % table.length;
+        HashMapEntry<K, V> entry = table[position];
+        HashMapEntry<K, V> last = null;
+        while (entry != null) {
+            if (Objects.equals(entry.key, key)) {
+                V oldValue = entry.value;
+                entry.value = value;
+                return oldValue;
+            }
+            last = entry;
+            entry = entry.next;
+        }
+        if (last == null) {
+            table[position] = new HashMapEntry<>(key, value);
+        } else {
+            last.next = new HashMapEntry<>(key, value);
+        }
+        size++;
+        return null;
+    }
+
+    @Override
+    public void putAll(Map<? extends K, ? extends V> map) {
+        ensureCapacity(size + map.size());
+        
+        
+    }
+
+    @Override
+    public V remove(Object key) {
+        int position = Objects.hashCode(key) % table.length;
+        HashMapEntry<K, V> entry = table[position];
+        HashMapEntry<K, V> last = null;
+        while (entry != null) {
+            if (Objects.equals(entry.key, key)) {
+               if (last == null) {
+                   table[position] = entry.next;
+               } else {
+                   last.next = entry.next;
+               }
+               size--;
+               return entry.value;
+            }
+            last = entry;
+            entry = entry.next;
+        }
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public Collection<V> values() {
+        throw new UnsupportedOperationException();
+    }
+    
+    private void ensureCapacity(int newSize) {
+        if (newSize <= table.length * loadFactor) {
+            return;
+        }
+        int newCapacity = Math.max(table.length * 2, newSize);
+        HashMapEntry[] newTable = new HashMapEntry[newCapacity];
+        
+        for (HashMapEntry<K, V> entry : table) {
+            while (entry != null) {
+                HashMapEntry<K, V> next = entry.next;
+                int newPosition = Objects.hashCode(entry.key) % newCapacity;
+                
+                entry.next = newTable[newPosition];
+                newTable[newPosition] = entry;
+                
+                entry = next;
+            }
+        }
+        
+        this.table = newTable;
+    }
+    
+    static final class HashMapEntry<K, V> implements Entry<K, V> {
+        final K key;
+        V value;
+        HashMapEntry<K, V> next;
+        
+        HashMapEntry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        @Override
+        public K getKey() {
+            return key;
+        }
+
+        @Override
+        public V getValue() {
+            return value;
+        }
+
+        @Override
+        public V setValue(V value) {
+            V oldValue = this.value;
+            this.value = value;
+            return oldValue;
+        }
+    }
 }
---
+++ src/test/java/com/hopkins/collections/HashMapTest.java
@@ -0,0 +1,152 @@
+package com.hopkins.collections;
+
+import static com.google.common.truth.Truth.assertThat;
+import org.junit.Before;
+import org.junit.Test;
+
+public class HashMapTest {
+    
+    private HashMap<String, Integer> map;
+    
+    @Before
+    public void setup() {
+        map = new HashMap<>();
+    }
+    
+    @Test
+    public void size_afterInit() {
+        assertThat(map.size()).isEqualTo(0);
+    }
+    
+    @Test
+    public void size_afterPut_increases() {
+        map.put("one", 1);
+        map.put("two", 2);
+        map.put("three", 3);
+        
+        assertThat(map.size()).isEqualTo(3);
+    }
+    
+    @Test
+    public void size_afterPutSameKey_doesNotIncrease() {
+        map.put("one", 1);
+        map.put("one", 2);
+        map.put("one", 3);
+        
+        assertThat(map.size()).isEqualTo(1);
+    }
+    
+    @Test
+    public void size_afterClear_returnsZero() {
+        map.put("one", 1);
+        map.clear();
+        
+        assertThat(map.size()).isEqualTo(0);
+    }
+    
+    @Test
+    public void isEmpty_afterInit_isTrue() {
+        assertThat(map.isEmpty()).isTrue();
+    }
+    
+    @Test
+    public void isEmpty_afterPut_isFalse() {
+        map.put("one", 1);
+        assertThat(map.isEmpty()).isFalse();
+    }
+    
+    @Test
+    public void get_afterPut_returnsValue() {
+        map.put("one", 1);
+        map.put("two", 2);
+        
+        assertThat(map.get("one")).isEqualTo(1);
+        assertThat(map.get("two")).isEqualTo(2);
+    }
+    
+    @Test
+    public void get_afterMultiplePut_returnsLastValue() {
+        map.put("one", 1);
+        map.put("one", 2);
+        
+        assertThat(map.get("one")).isEqualTo(2);
+    }
+    
+    @Test
+    public void put_returnsOldValue() {
+        assertThat(map.put("one", 1)).isNull();
+        assertThat(map.put("one", 2)).isEqualTo(1);
+    }
+    
+    @Test
+    public void get_afterInit_returnsNull() {
+        assertThat(map.get("one")).isNull();
+    }
+    
+    @Test
+    public void containsKey_afterPut() {
+        map.put("one", 1);
+        map.put("two", 2);
+        
+        assertThat(map.containsKey("one")).isTrue();
+        assertThat(map.containsKey("two")).isTrue();
+        assertThat(map.containsKey("three")).isFalse();
+    }
+    
+    @Test
+    public void containsValue() {
+        map.put("one", 1);
+        map.put("two", 2);
+        
+        assertThat(map.containsValue(1)).isTrue();
+        assertThat(map.containsValue(2)).isTrue();
+        assertThat(map.containsValue(3)).isFalse();
+    }
+    
+    @Test
+    public void remove_returnsValue() {
+        map.put("one", 1);
+        map.put("two", 2);
+        
+        assertThat(map.remove("one")).isEqualTo(1);
+        assertThat(map.remove("two")).isEqualTo(2);
+        assertThat(map.remove("three")).isNull();
+    }
+    
+    @Test
+    public void init_withHighLoadFactor_works() {
+        map = new HashMap<>(2 /* initialCapacity */, 4.0F /* loadFactor */);
+        map.put("one", 1);
+        map.put("two", 2);
+        map.put("three", 3);
+        map.put("four", 4);
+        
+        assertThat(map.size()).isEqualTo(4);
+        assertThat(map.capacity()).isEqualTo(2);
+        assertThat(map.get("one")).isEqualTo(1);
+        assertThat(map.get("two")).isEqualTo(2);
+        assertThat(map.get("three")).isEqualTo(3);
+        assertThat(map.get("four")).isEqualTo(4);
+        
+        assertThat(map.containsKey("one")).isTrue();
+        assertThat(map.containsKey("five")).isFalse();
+    }
+    
+    @Test
+    public void capacity_afterInit_returnsDefault() {
+        assertThat(map.capacity()).isEqualTo(16);
+    }
+    
+    @Test
+    public void capacity_afterPut_increases() {
+        map = new HashMap<>(2 /* initialCapacity */);
+        map.put("one", 1);
+        map.put("two", 2);
+        map.put("three", 2);
+        
+        assertThat(map.capacity()).isEqualTo(4);
+        assertThat(map.size()).isEqualTo(3);
+        assertThat(map.get("one")).isEqualTo(1);
+        assertThat(map.get("two")).isEqualTo(2);
+    }
+}
