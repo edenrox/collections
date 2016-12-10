8234e270e195c873e7686f4790137ef5d355acd9
Ian Hopkins <ian@lucidhelix.com>
2016-12-10 09:57:51 -0800

    Adding the empty collections.

---
+++ src/main/java/com/hopkins/collections/EmptyIterator.java
@@ -0,0 +1,22 @@
+package com.hopkins.collections;
+
+/** 
+ * An implementation of {@link Iterator} that is empty and unmodifiable.
+ */
+final class EmptyIterator<T> implements Iterator<T> {
+
+    @Override
+    public boolean hasNext() {
+        return false;
+    }
+
+    @Override
+    public T next() {
+        throw new NoSuchElementException();
+    }
+
+    @Override
+    public void remove() {
+        throw new UnsupportedOperationException();
+    }
+}
